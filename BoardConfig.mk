f70fd1290527b59dd54720a975a018c33cf6b5e9
fefifofum <jorsellis@gmail.com>
2016-02-06 16:09:52 +0100

    Remove deprecated flag
    Change-Id: Ifc9daed67a7bd433fe47c3da60bd855a306c08bb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,4 +45,3 @@ TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
